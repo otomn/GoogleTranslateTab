76315fdd56d3eb20c86e471a95a83167f7c6bb82
Zhenyao She <dr.tobyshe@gmail.com>
2020-08-22 19:39:10 -0400

    Added simple menu bar app

---
+++ GoogleTranslateTab/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  GoogleTranslateTab
+//
+//  Created by Toby on 2020-08-02.
+//  Copyright © 2020 Toby. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        
+        let contentView = ContentView(translateManager: translateManagerSetup())
+        
+        popover = NSPopover()
+        popover.contentSize = NSSize(width: 600, height: 500)
+        popover.behavior = .applicationDefined
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.imageScaling = .scaleProportionallyDown
+            button.action = #selector(togglePopover)
+        }
+    }
+    
+    // Create the status item
+    @objc func togglePopover(_ sender: AnyObject?) {
+         if let button = statusBarItem.button {
+              if popover.isShown {
+                   popover.performClose(sender)
+              } else {
+                   popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+              }
+         }
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
+func translateManagerSetup() -> GoogleTranslate{
+    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "GoogleApiKey") as? String else {
+        fatalError("Cannot find GoogleApiKey in Info.plist")
+    }
+    let translate = GoogleTranslate(apiKey: apiKey, userLanguage: Locale.preferredLanguages[0])
+    translate.supportedLanguages[.base] = ["en": "Engligh", "zh": "Chinese"]
+    return translate
+}
---
+++ GoogleTranslateTab/ContentView.swift
@@ -0,0 +1,172 @@
+//
+//  ContentView.swift
+//  GoogleTranslateTab
+//
+//  Created by Toby on 2020-08-02.
+//  Copyright © 2020 Toby. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var loaded = false
+    @State private var translateFrom = ""
+    @State private var translateTo = ""
+    @State private var fromLan = "en"
+    @State private var toLan = "en"
+    @State private var detectedLan = ""
+    @State private var languagesMap: [String: String] = [:]
+    @State private var model: GoogleTranslate.TranslateModel = .base
+    @State private var startOnBoot = false
+    
+    var translateManager: GoogleTranslate
+    
+    var languageCodes: [String] {
+        languagesMap.keys.sorted()
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                
+                Picker(selection: $fromLan, label: Text("From")) {
+                    ForEach(0 ..< languageCodes.count, id: \.self){
+                        Text(self.languagesMap[self.languageCodes[$0]]!)
+                            .tag(self.languageCodes[$0])
+                    }
+                    Text("Auto Detect" + 
+                        (detectedLan == "" ? "" : " (\(detectedLan))")).tag("auto")
+                    }.disabled(!loaded).id(languagesMap)
+                
+                Button(action: {
+                    if self.fromLan != "auto" {
+                        let temp = self.fromLan
+                        self.fromLan = self.toLan
+                        self.toLan = temp
+                    }
+                }, label: { Text("􀄭") }).disabled(!loaded)
+                
+                Picker(selection: $toLan, label: Text("To")) {
+                    ForEach(0 ..< languageCodes.count, id: \.self){
+                        Text(self.languagesMap[self.languageCodes[$0]]!)
+                            .tag(self.languageCodes[$0])
+                    }.id(languagesMap)
+                }.disabled(!loaded)
+                
+                Button(action: {
+                    self.savePref()
+                    self.translateStart()
+                }, label: { Text("Translate") }).disabled(!loaded)
+                
+                MenuButton("•••"){
+                    MenuButton("Model"){
+                        Toggle(isOn: Binding(get: { 
+                            self.model == GoogleTranslate.TranslateModel.nmt
+                        }, set: {
+                            if $0 {
+                                self.model = GoogleTranslate.TranslateModel.nmt
+                                self.reloadLanguages()
+                            }
+                        })) {
+                            Text("Neural Machine Translation")
+                        }
+                        Toggle(isOn: Binding(get: { 
+                            self.model == GoogleTranslate.TranslateModel.base
+                        }, set: {
+                            if $0 {
+                                self.model = GoogleTranslate.TranslateModel.base
+                                self.reloadLanguages()
+                            }
+                        })) {
+                            Text("Phrase-Based Machine Translation")
+                        }
+                    }
+                    Button(action: {
+                        NSApplication.shared.terminate(self)
+                    }, label: { Text("Quit") })
+                }
+                .scaledToFit()
+            }
+            TextField("Translate From", text: $translateFrom).disabled(!loaded)
+            TextField("Translate To", text: $translateTo)
+        }
+        .padding(.all, 10.0)
+        .onAppear(perform: onload)
+    }
+    
+    private static let fromlanKey = "fromLan"
+    private static let tolanKey = "toLan"
+    private static let modelKey = "model"
+    
+    private func reloadLanguages(){
+        self.languagesMap = self.translateManager.supportedLanguages[self.model]!
+    }
+    
+    private func translateStart(){
+        var translateParam = translateManager.defaultTranslateParams()
+        if fromLan != "auto" {
+            translateParam.source = fromLan
+        }
+        translateParam.target = toLan
+        translateParam.text = [translateFrom]
+        translateParam.model = model
+        translateTo = "(Translating)"
+        print(translateParam)
+        translateManager.translate(params: translateParam, 
+                                   errorHandler: handleError,
+                                   resultHandler: handleResult)
+    }
+    
+    private func handleError(error: String){
+        NSLog(error)
+        translateTo = error.hasPrefix("Error requesting")
+            ? "(Network Error)"
+            : "(Unknown Error)"
+    }
+    
+    private func handleResult(results: [GoogleTranslate.TranslateResponseTranslation]){
+        if let result = results.first {
+            detectedLan = languagesMap[result.detectedSourceLanguage ?? ""] ?? ""
+            translateTo = result.translatedText
+        }
+    }
+    
+    private func onload(){
+        loadPref()
+        translateManager.loadSupportedLanguages(onComplete: {
+            for m in self.translateManager.supportedLanguages{
+                var loaded: [String] = []
+                for l in m.value {
+                    if loaded.contains(l.value){
+                        self.translateManager.supportedLanguages[m.key]![l.key] = nil
+                    }
+                    loaded.append(l.value)
+                }
+            }
+            self.loaded = true
+            self.reloadLanguages()
+        }, errorHandler: handleError)
+    }
+    
+    private func loadPref(){
+        let prefs = UserDefaults.standard
+        fromLan = prefs.string(forKey: ContentView.fromlanKey) ?? "en"
+        toLan = prefs.string(forKey: ContentView.tolanKey) ?? "en"
+        model = GoogleTranslate.TranslateModel(
+            prefs.string(forKey: ContentView.modelKey) ?? "") ?? .base
+    }
+    
+    private func savePref(){
+        let prefs = UserDefaults.standard
+        prefs.set(fromLan, forKey: ContentView.fromlanKey)
+        prefs.set(toLan, forKey: ContentView.tolanKey)
+        prefs.set(self.model.description, forKey: ContentView.modelKey)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(translateManager: translateManagerSetup())
+    }
+}
